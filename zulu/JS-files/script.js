fac5bbba91138f5dfeb80802618b16840059adfb
Choco <123357954+chocolocoboo@users.noreply.github.com>
2024-02-14 15:05:52 +0100

    kuken är giga

--- zulu/JS-files/script.js
+++ zulu/JS-files/script.js
@@ -1,3 +1,10 @@
-function myFunction() {
-   
-  }
+
+function ScrollVinyl() 
+{
+  const vinyl = document.getElementById("test");
+  console.log("hghg");
+  vinyl.style.backgroundColor = "red";
+  vinyl.style.transform = "rotate(5deg)";
+}
+
+console.log("HEJSAN");
