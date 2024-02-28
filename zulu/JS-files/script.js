955a7bf7195510f202f13e866e7600a4fbd3c34d
Choco <123357954+chocolocoboo@users.noreply.github.com>
2024-02-28 12:37:35 +0100

    fixed page change way

--- zulu/JS-files/script.js
+++ zulu/JS-files/script.js
@@ -1,10 +1,33 @@
-
+let degree = 0;
+let transper= 0;
+let scrollPosition = 0;
+var vinyl = document.getElementById("vinyl-record");
+var stickt = 0;
 function ScrollVinyl() 
 {
-  const vinyl = document.getElementById("test");
-  console.log("hghg");
-  vinyl.style.backgroundColor = "red";
-  vinyl.style.transform = "rotate(5deg)";
+  FixedVinyl();
+  if (scrollPosition > window.scrollY)
+  {
+    degree += 8;
+  }
+  else if (scrollPosition < window.scrollY)
+  {
+    degree -= 8;
+  }
+  scrollPosition = window.scrollY;
+
+  const vinyl = document.getElementById("vinyl-record");
+  vinyl.style.transform = "rotate(" + degree + "deg)";
 }
 
+
 console.log("HEJSAN");
+
+function FixedVinyl(){
+  if(window.scrollY >= sticky) {
+    vinyl.classList.add("sticky")
+  } else {
+    vinyl.classList.remove("sticky");
+  }
+
+}
