6e8a864a2ce1355b3487641e7843354758d4bb5c
Choco <123357954+chocolocoboo@users.noreply.github.com>
2024-03-06 13:46:25 +0100

    fade in + out lp skiva

--- zulu/JS-files/script.js
+++ zulu/JS-files/script.js
@@ -2,7 +2,10 @@ let degree = 0;
 let transper= 0;
 let scrollPosition = 0;
 var vinyl = document.getElementById("vinyl-record");
-var stickt = 0;
+var sticky = 0;
+let opNum = 0;
+
+
 function ScrollVinyl() 
 {
   FixedVinyl();
@@ -18,16 +21,51 @@ function ScrollVinyl()
 
   const vinyl = document.getElementById("vinyl-record");
   vinyl.style.transform = "rotate(" + degree + "deg)";
+  
+  vinylOpacity();
+  setTimeout(()=> {
+    fadeOut();
+  }, 700)
 }
 
 
-console.log("HEJSAN");
+
+
 
 function FixedVinyl(){
   if(window.scrollY >= sticky) {
     vinyl.classList.add("sticky")
   } else {
-    vinyl.classList.remove("sticky");
-  }
+    vinyl.classList.remove("sticky")
+}
+}
 
+const element = document.querySelector("div#libbsticka");
+const output = document.querySelector("p#output");
+
+/*element.onscroll=(event)=> {
+  vinylOpacity();
+  setTimeout(()=> {
+    fadeOut();
+  }, 1000)
+  
+}*/
+
+function vinylOpacity()
+{
+  opNum = 1;
+  vinyl.style.opacity = opNum ;
+ 
+}
+const delay = ms => new Promise(res => setTimeout(res, ms));
+async function fadeOut()
+{
+  
+  while( opNum > 0)
+  {
+    await delay(50);
+    opNum -= 0.01;
+    vinyl.style.opacity = opNum ;
+  }
+  
 }
