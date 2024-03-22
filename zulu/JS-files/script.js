b19d895093f9895fa515aace1a76eec635664601
Choco <123357954+chocolocoboo@users.noreply.github.com>
2024-03-22 11:59:18 +0100

    idk

--- zulu/JS-files/script.js
+++ zulu/JS-files/script.js
@@ -68,3 +68,13 @@ async function fadeOut()
   }
   
 }
+
+
+function myFunction() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
